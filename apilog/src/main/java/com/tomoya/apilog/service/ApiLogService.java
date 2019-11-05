ff82bc40e5f4ef50af0712bbf5eb796145156b95
baoyuan.zhang <baoyuan.zhang@itrigger.com>
2019-11-05 19:43:11 +0800

    去除无用代码

--- apilog/src/main/java/com/tomoya/apilog/service/ApiLogService.java
+++ apilog/src/main/java/com/tomoya/apilog/service/ApiLogService.java
@@ -5,8 +5,4 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ApiLogService {
     void saveLog(String type, String request, String response, String error);
-    void testEx();
-
-    String changBindPhone(String oldPhone, String newPhone, String code, String bizUserId);
-
 }
